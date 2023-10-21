26463542bfa8876a7de8c734193f5af11b9df517
Elle0-0 <andreamichellevaz@gmail.com>
2023-10-21 21:52:50 +0100

    added initial GUI code

--- src/quizgame/Cli.java
+++ src/quizgame/Cli.java
@@ -1,7 +1,49 @@
 package quizgame;
 
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
 public class Cli {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+   // public static void main(String[] args) {
+     //   System.out.println("Hello World");
+       // User u = new User();
+        
+       // u.answeredQuestions[0] = QuestionGetter.getRandomQuestion();
+
+    //}
+
+    public class Main extends Application {
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+        @Override
+        //public void start(Stage arg0) throws Exception {
+            // TODO Auto-generated method stub
+        //    throw new UnsupportedOperationException("Unimplemented method 'start'");    
+        //}
+        public void start(Stage primaryStage) {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("mainpage.fxml"));
+                Scene scene = new Scene(root);
+                //scene.getStylesheets().add(getClass().getResource(""));
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+
+
     }
+
+
 }

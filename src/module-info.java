2496ed09f7c19b922c4b2b8005da3bd84c0fd1da
Cormac Shannon <cormacpshannon@gmail.com>
2023-10-24 10:48:27 +0100

    Remove bloat from module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,12 +1,7 @@
 module EPIC.Quiz {
-
     requires javafx.controls;
     requires javafx.fxml;
-
-
-
+    
     opens quizgame to javafx.fxml;
     exports quizgame;
-    //exports quizgame.resource;
-    opens quizgame.resource to javafx.fxml;
 }

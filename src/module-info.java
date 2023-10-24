e93d8a3eaf33973bd38a851a19198abff2c266ff
Cormac Shannon <cormacpshannon@gmail.com>
2023-10-24 14:50:11 +0100

    First version of Jar build system completed

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,12 @@
-module EPIC.Quiz {
+module quizgame {
+    requires javafx.graphics;
     requires javafx.controls;
+    requires javafx.media;
+    requires javafx.base;
+    requires javafx.web;
+    requires javafx.swing;
     requires javafx.fxml;
-    
+
     opens quizgame to javafx.fxml;
     exports quizgame;
 }
